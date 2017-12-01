061665983dca44e14885b5a45df3d1adf0d2a0b1
dougbthompson <doug.askaar@gmail.com>
2017-12-01 09:08:05 -0800

    20171201 09:08

--- query/queries.sql
+++ query/queries.sql
@@ -1,3 +1,6 @@
 
 select source_type, count(1) from drgn_audit group by 1 order by 2 desc;
 
+select round(avg(eth),6), round(avg(perc),6), avg(dragons), sum(dragons) from drgn_audit where dragons < 10000000;
+select round(avg(eth),6), round(avg(perc),6), avg(dragons), sum(dragons) from drgn_audit;
+
